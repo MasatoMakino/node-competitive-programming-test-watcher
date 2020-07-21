fd811613e558ab1e7ddf8a7bb44a3673b0bd949d
MasatoMakino <unagiinu.g@gmail.com>
2020-07-21 13:00:21 +0900

    add : setting CI and local #6

--- jest.local.config.js
+++ jest.local.config.js
@@ -142,10 +142,10 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-      "/node_modules/",
-    "_auth_"
-  ],
+  // testPathIgnorePatterns: [
+  //     "/node_modules/",
+  //   "_auth_"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
